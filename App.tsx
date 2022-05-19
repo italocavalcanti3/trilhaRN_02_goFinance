983b19247a1063d933923c902ee2bba1b3f8a08b
Italo Cavalcanti <87244758+italocavalcanti3@users.noreply.github.com>
2022-05-19 18:51:02 -0300

    adicionando tab-navigation e criando arquivo de rotas, adicionando as 3 telas do projeto

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import AppLoading from 'expo-app-loading';
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
 
 import {
   useFonts,
@@ -11,7 +11,8 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
-import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,9 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
